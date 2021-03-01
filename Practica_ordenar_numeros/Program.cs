1ecf21e5ad6352f0edb5a2d4021252d07aeabcb8
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 16:56:43 -0400

    Add files via upload
    Un programa que pida al usuario 5 números reales (pista: necesitarás un array de "float") y luego los muestre en el orden contrario al que se introdujeron.

---
+++ Practica_ordenar_numeros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practica_ordenar_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numeros = new float[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Inserte un numero:");
+                string valor = Console.ReadLine();
+                numeros[i] = float.Parse(valor);
+            }
+
+            Console.WriteLine("Resultado:");           
+            for (int i = 4; i >=0; i--)
+            {
+                Console.WriteLine("Numero " + i + ":" + numeros[i]);                
+            }
+        }
+    }
+}
